a191ce032e2a54fa5e7f22e01398733503d0f2a8
JornVerheggen <jorn-v@hotmail.com>
2022-05-01 22:31:55 +0200

    solver working on null_test

--- solver.py
+++ solver.py
@@ -1,92 +1,73 @@
-
-# %%
 import numpy as np
-import math as m
-import cmath
 
 
 def normalizeInputData(xyz, multiplier=50000) -> np.array:
     xyzPosNeg = np.absolute(xyz)/xyz
     xyz = 1/np.sqrt(np.absolute(xyz)) * xyzPosNeg * multiplier
 
-
 def transformToRefrenceFrame(xyz, transformMatrix) -> np.array:
-    return xyz @ transformMatrix
+    return transformMatrix @ xyz
 
+def getScaleFactor(sa, vsama, ma, sb, vsbmb, mb) -> np.array:
+    return (-sa - (mb-ma) + sb) / (vsama-vsbmb)
 
-def getScale(sa, vsama, ma, sb, vsbmb, mb) -> np.array:
+def getMeanScaleFactor(sf12,sf23,sf31) -> float:
+    return np.nanmean(np.concatenate((sf12,sf23,sf31)))
 
-    print(vsama)
-    dmamb = np.sqrt(np.sum(np.square(ma-mb)))
-    #dmamb = 81.404
-    print(dmamb)
+def getMagPosition(sensorPos, vector, sf) -> np.array:
+    return sensorPos + vector * sf
 
-    sxdif = sb[0] - sa[0]
-    sydif = sb[1] - sa[1]
-    szdif = sb[2] - sa[2]     
+def getTranslation(m1, m2, m3) -> np.array:
+    return  (m1+m2+m3) / 3
 
-    vxdif = vsbmb[0] - vsama[0]
-    vydif = vsbmb[1] - vsama[1]
-    vzdif = vsbmb[2] - vsama[2]    
+def getRotation(m1, m2, m3) -> np.array:
 
-    print(f'here{vxdif}')
-    #ABC formula
+    N = np.cross((m1-m3),(m2-m3))
+    U = N/ np.linalg.norm(N)
 
-    a = 3
+    return np.pi/2 - np.arccos(U)
 
-    b = 6 * (sxdif*vxdif+sydif*vydif+szdif*vzdif) + 6 *(vxdif + vydif + vzdif)
-
-    c = - cmath.sqrt(dmamb) + sxdif**2 + sydif**2 + szdif**2 + vxdif**2 + vydif**2 + vzdif**2
-
-    d = (b**2) - (4*a*c)
-
-    # find two solutions
-    sol1 = (-b-cmath.sqrt(d))/(2*a)
-    sol2 = (-b+cmath.sqrt(d))/(2*a)
-
-    print(f'A is {a}, B is {b} and C is{c}')
-    print('The solution are {0} and {1}'.format(sol1,sol2))
-
-
-def getMagPosition(xyz, pos) -> np.array:
-    return xyz + pos
-
-
-def getTransrot(xyz1, xyz2, xyz3):
-    pass
-
-def getTransmat(num) -> np.array:
-    if num == 1:
+def getRotMat(num) -> np.array:
+    if num == 1: # -90deg y-axis -> 180deg z-axis
+        return np.array([
+            [np.cos(-np.pi/2),0,np.sin(-np.pi/2)],
+            [0,1,0],
+            [-np.sin(-np.pi/2),0,np.cos(-np.pi/2)]]) @ \
+            np.array(
+            [[np.cos(np.pi), -np.sin(np.pi), 0],
+            [np.sin(np.pi),   np.cos(np.pi), 0],
+            [0., 0., 1.]])
+    elif num == 2: #120deg z-axis -> -90deg y-axais
         return np.array(
-        [[np.cos(np.pi), -np.sin(np.pi), 0],
-        [np.sin(np.pi),   np.cos(np.pi), 0],
-        [0., 0., 1.]])
-
-    elif num == 2:
+            [[np.cos(np.pi/1.5), -np.sin(np.pi/1.5), 0],
+            [np.sin(np.pi/1.5),   np.cos(np.pi/1.5), 0],
+            [0., 0., 1.]]) @ \
+            np.array([
+            [np.cos(-np.pi/2),0,np.sin(-np.pi/2)],
+            [0,1,0],
+            [-np.sin(-np.pi/2),0,np.cos(-np.pi/2)]])
+    elif num == 3: #-120deg z-axis -> -90deg y-axis
         return np.array(
-        [[np.cos(np.pi/3), -np.sin(np.pi/3), 0],
-        [np.sin(np.pi/3),   np.cos(np.pi/3), 0],
-        [0., 0., 1.]])
-
-    elif num == 3: 
-        return np.array(
-        [[np.cos(-np.pi/3), -np.sin(-np.pi/3), 0],
-        [np.sin(-np.pi/3),   np.cos(-np.pi/3), 0],
-        [0., 0., 1.]])
+            [[np.cos(-np.pi/1.5), -np.sin(-np.pi/1.5), 0],
+            [np.sin(-np.pi/1.5),   np.cos(-np.pi/1.5), 0],
+            [0., 0., 1.]]) @ \
+            np.array([
+            [np.cos(-np.pi/2),0,np.sin(-np.pi/2)],
+            [0,1,0],
+            [-np.sin(-np.pi/2),0,np.cos(-np.pi/2)]])
 
 def getSp(num):
     if num == 1:
         return np.array([-34.2, 0, 0])
     elif num == 2:
-        return np.array([17.1, 29.618, 0])
+        return np.array([17.1, -29.6180688, 0])
     elif num == 3:
-        return np.array([17.1, -29.618, 0])
+        return np.array([17.1, 29.6180688, 0])
 
 def getMp(num):
     if num == 1:
         return np.array([-47, 0, 0])
     elif num == 2:
-        return np.array([23.5, 40.7, 0])
+        return np.array([23.5, -40.7031939, 0])
     elif num == 3:
-        return np.array([23.5, -40.7, 0])
-
+        return np.array([23.5, 40.7031939, 0])
--- test_solver.py
+++ test_solver.py
@@ -1,7 +1,7 @@
 from socket import getservbyport
 import unittest
 import numpy as np
-from solver import normalizeInputData, transformToRefrenceFrame, getScale, getMagPosition, getTransrot, getTransmat, getMp, getSp
+from solver import normalizeInputData, transformToRefrenceFrame, getScaleFactor, getMeanScaleFactor, getMagPosition, getTranslation, getRotation, getRotMat, getMp, getSp
 
 class TestModule(unittest.TestCase):
     # Test where all the sensor values indicate that the magnets is 10 units in the z axis
@@ -13,15 +13,40 @@ class TestModule(unittest.TestCase):
 
         #skip data normalization
 
-        vs1m1 = transformToRefrenceFrame(s1Val,getTransmat(1))
-        vs2m2 = transformToRefrenceFrame(s2Val,getTransmat(2))
-        vs3m3 = transformToRefrenceFrame(s3Val,getTransmat(3))
+        vs1m1 = transformToRefrenceFrame(s1Val,getRotMat(1))
+        vs2m2 = transformToRefrenceFrame(s2Val,getRotMat(2))
+        vs3m3 = transformToRefrenceFrame(s3Val,getRotMat(3))
 
-        print(vs1m1)
-        self.assertAlmostEqual(vs1m1.any(),np.array([-10,0,0]).any())
-        self.assertAlmostEqual(vs2m2.all(),np.array([8.660,-5,0]).all())
-        self.assertAlmostEqual(vs3m3.all(),np.array([8.660,5,0]).all())
+        np.testing.assert_array_almost_equal(vs1m1,np.array([-10,0,0]))
+        np.testing.assert_array_almost_equal(vs2m2,np.array([5,-8.660254,0]))
+        np.testing.assert_array_almost_equal(vs3m3,np.array([5, 8.660254,0]))
 
-        sf12 = getScale(getSp(1),vs1m1,getMp(1),getSp(2),vs2m2,getMp(2))
+        sf12 = getScaleFactor(getSp(1),vs1m1,getMp(1),getSp(2),vs2m2,getMp(2))
+        sf23 = getScaleFactor(getSp(2),vs2m2,getMp(2),getSp(3),vs3m3,getMp(3))
+        sf31 = getScaleFactor(getSp(3),vs3m3,getMp(3),getSp(1),vs1m1,getMp(1))
 
-        print("sf12 moet volgens mij zijn: 1.28")
+        np.testing.assert_array_almost_equal(sf12,np.array([1.28,1.28,np.nan]))
+        np.testing.assert_array_almost_equal(sf23,np.array([np.nan,1.28,np.nan]))
+        np.testing.assert_array_almost_equal(sf31,np.array([1.28,1.28,np.nan]))
+
+        sf = getMeanScaleFactor(sf12,sf23,sf31)
+        self.assertAlmostEqual(sf,1.28)
+
+        m1Pos = getMagPosition(getSp(1),vs1m1,sf)
+        m2Pos = getMagPosition(getSp(2),vs2m2,sf)
+        m3Pos = getMagPosition(getSp(3),vs3m3,sf)
+
+        #Note this is only true in the case that the outer ring has no rotation or translation
+        np.testing.assert_array_almost_equal(m1Pos,getMp(1))
+        np.testing.assert_array_almost_equal(m2Pos,getMp(2))
+        np.testing.assert_array_almost_equal(m3Pos,getMp(3))
+
+        outerPos = getTranslation(m1Pos,m2Pos,m3Pos)
+
+        np.testing.assert_array_almost_equal(outerPos,np.array([0,0,0]))
+
+        outerRot = getRotation(m1Pos,m2Pos,m3Pos)
+        print(outerRot)
+
+
+
